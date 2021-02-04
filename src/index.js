3f5f255d2af4273a1e4b1413a663ab3f6cb7b103
Dionei Bianchati <dion.biancha@gmail.com>
2021-02-04 11:45:32 -0300

    List project API

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import { View, StyleSheet, Text, StatusBar } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { 
+  SafeAreaView, 
+  StyleSheet, 
+  Text, 
+  StatusBar,
+  FlatList 
+} from 'react-native';
+
+import api from './services/api';
 
 export default function App() {
+  const [projects, setProjects] = useState([]);
+
+  useEffect(() => {
+    api.get('projects').then(response => {
+      console.log(response.data);
+      setProjects(response.data);
+    });
+  }, []);
+
+
   return (
     <>
       <StatusBar  backgroundColor="#7159c1"/>
-      <View style={styles.container}>
-        <Text style={styles.title}>Hello GoStack</Text>
-      </View>
+      <SafeAreaView style={styles.container}>
+        <FlatList 
+          data={projects}
+          keyExtractor={project => project.id}
+          renderItem={({item: project})=> (
+            <Text style={styles.project}>{project.title}</Text>
+          )}
+        />
+      </SafeAreaView>
     </>
   );
 } 
@@ -16,12 +40,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#7159c1',
-    justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    justifyContent: 'center'
   },
   title: {
     fontSize: 32,
     color: '#FFF',
     fontWeight: 'bold'
+  },
+  project: {
+    color: '#FFF',
+    fontSize: 20
   }
 });
